afe11d2008d93ba7c91e96839edbb2b9cf13c506
Maanas Vohra <maanasvohra1998@gmail.com>
2018-02-08 23:07:53 +0000

    Added database(mongoose)

--- v1/app.js
+++ v1/app.js
@@ -1,18 +1,47 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+// connecting mongoose to the main app
+mongoose.connect("mongodb://localhost/bibliophile");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var books = [
-        {name: "Salmon Creek", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
-        {name: "Midnight Meadows", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
-        {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
-        {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
-        {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
-        {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
-        {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"}
-    ];
+// making the schema for the book
+var bookSchema = new mongoose.Schema({
+    bookName: String,
+    bookImage: String,
+    description: String
+});
+
+// making the model for the book
+var Book = mongoose.model("Book", bookSchema);
+
+// Book.create(
+//     {
+//         bookName: "To Kill A Mockingbird",
+//         bookImage: "https://upload.wikimedia.org/wikipedia/en/thumb/7/79/To_Kill_a_Mockingbird.JPG/220px-To_Kill_a_Mockingbird.JPG",
+//         description: "An indisputable classic."
+//     },
+//     function(err, book) {
+//         if(err) {
+//             console.log("Error creating book");
+//         } else {
+//             console.log(book);
+//         }
+//     }
+// );
+
+// var books1 = [
+//         {name: "Salmon Creek", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
+//         {name: "Midnight Meadows", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
+//         {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
+//         {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
+//         {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
+//         {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"},
+//         {name: "Swedish Salmonella", image: "https://media-cdn.tripadvisor.com/media/photo-s/03/8f/90/23/elkmont-campground.jpg"}
+//     ];
 
 // the main landing page
 app.get("/", function(req, res){
@@ -21,15 +50,35 @@ app.get("/", function(req, res){
 
 // the campsites page -> get the page
 app.get("/books", function(req, res){
-    res.render("books.ejs", {books: books});
+    // get all the books
+    Book.find({}, function(err, allBooks){
+        if(err) {
+            console.log("Error finding the books");
+        }   else {
+            console.log(allBooks);
+            res.render("index.ejs", {books: allBooks});
+        }
+    })
 });
 
 app.post("/books", function(req, res){
     // get data from form, add to books array
     var name = req.body.name;
     var image = req.body.image;
-    var newBook = {name: name, image: image};
-    books.push(newBook);
+    var description = req.body.description;
+    Book.create(
+        {
+            bookName: name,
+            bookImage: image,
+            description: description
+        }, function(err, book) {
+            if(err) {
+                console.log("error adding book");
+            } else {
+                console.log("Book added!");
+            }
+        }
+    );
     // redirect to books page
     res.redirect("/books");
 });
@@ -37,6 +86,17 @@ app.post("/books", function(req, res){
 // create the form here, this is the RESTful way of writing 
 app.get("/books/new", function(req, res){
     res.render("new.ejs");
+});
+
+// display page for the individual book
+app.get("/books/:id", function(req, res){
+    Book.findById(req.params.id, function(err, foundBook) {
+        if(err) {
+            console.log("Couldn't find the book");
+        } else {
+            res.render("show.ejs", {book: foundBook})
+        }
+    })
 });
 
 // listen the request
